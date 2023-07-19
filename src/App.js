c5d5277a66521dd49c02897ddc4d567371452012
Sanesty-11 <sanesty11@gmail.com>
2023-07-19 17:11:45 -0400

    Second Commit, added list card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Card from "./Card";
 
 function App() {
-  return <div className="App">Sample React Page</div>;
+  return (
+    <div className="App">
+      Sample React Page
+      <Card />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,15 @@
+function Card() {
+  return (
+    <div className="card">
+      <div className="title">Things to Do:</div>
+      <ol className="list_container">
+        <li className="list_item">first thing</li>
+        <li className="list_item">second</li>
+        <li className="list_item">third</li>
+        <li className="list_item">fourth</li>
+      </ol>
+    </div>
+  );
+}
+
+export default Card;
